d4a22c893fdaf91cc652143f1fa7d1fbd559c619
jgu7man <jgu7man@gmail.com>
2021-06-13 19:25:18 -0500

    import dialog

--- projects/marxa/auth/src/public-api.ts
+++ projects/marxa/auth/src/public-api.ts
@@ -5,3 +5,4 @@
 export * from './lib/auth.service';
 export * from './lib/auth.module';
 export * from './lib/auth.model';
+export * from './lib/components/restore-password-dialog/restore-password.dialog'

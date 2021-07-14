608e99a69c94e7bae36ae4db1457907d9cc423fc
jgu7man <jgu7man@gmail.com>
2021-07-14 15:45:50 -0500

    storage version 0.0.19

--- projects/marxa/storage/src/public-api.ts
+++ projects/marxa/storage/src/public-api.ts
@@ -7,3 +7,6 @@ export * from './lib/mx-storage.module';
 export * from './lib/mx-storage.model';
 export * from './lib/components/upload-modal/upload-modal.component'
 export * from './lib/components/uploading-spinner/uploading-spinner.component'
+export * from './lib/components/import-dialog/import.dialog'
+export * from './lib/components/rename-columns/rename-columns.dialog'
+export * from './lib/components/import-file/error-columns-required/error-columns-required.dialog'

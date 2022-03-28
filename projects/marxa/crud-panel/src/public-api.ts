d7c5ca35a359f5791032910b98745d1ec02f2b20
jorge gu7man <jgu7man@gmail.com>
2022-03-28 17:17:12 -0600

    borrado crud panel para mover de directorio

--- projects/marxa/crud-panel/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of crud-panel
- */
-
-export * from './lib/mx-crud-panel.service';
-export * from './lib/mx-crud-panel.module';
-export * from './lib/components/mx-crud-panel/mx-crud-panel.component'

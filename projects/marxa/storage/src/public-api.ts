d73bc70b389eb55324d4972173ab15409f59e776
jorge gu7man <jgu7man@gmail.com>
2022-06-29 10:27:34 -0500

    Create table importer

--- projects/marxa/storage/src/public-api.ts
+++ projects/marxa/storage/src/public-api.ts
@@ -2,7 +2,7 @@
  * Public API Surface of mx-storage
  */
 
-export * from './lib/mx-storage.service';
+export * from './lib/services/mx-storage.service';
 export * from './lib/mx-storage.module';
 export * from './lib/mx-storage.model';
 export * from './lib/components/upload-modal/upload-modal.component'

ad4402912835814fa4d064f62d8ed910db56b9f7
jorge gu7man <jgu7man@gmail.com>
2022-08-08 23:09:45 -0500

    auth submodule removed

--- projects/marxa/auth/src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of auth
- */
-
-export * from './lib/auth.service';
-export * from './lib/auth.module';
-export * from './lib/auth.model';
-export * from './lib/components/restore-password-dialog/restore-password.dialog'

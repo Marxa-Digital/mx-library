0ab6e8d997584d68033eb8b05189f9212f7be3ec
Jorge Gu7man <46007655+jgu7man@users.noreply.github.com>
2023-01-15 14:03:32 -0600

    ⛔️ Sidenave migrated to mx-modules

--- projects/marxa/sidenav/src/lib/mx-sidenav.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface MxSidenavNode {
-  name: string,
-  routeId?: string | string[],
-  route?: string,
-  childs?: MxSidenavNode[],
-  disable?: boolean
-}
--- projects/marxa/sidenav/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of sidenav
- */
-
-export * from './lib/mx-sidenav.service';
-export * from './lib/mx-sidenav.component';
-export * from './lib/mx-sidenav.module';

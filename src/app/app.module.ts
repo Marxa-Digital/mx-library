9502a9d31eada253b1c5093aea996726c19e6bef
Jorge Gu7man <46007655+jgu7man@users.noreply.github.com>
2023-01-15 14:04:35 -0600

    🛠 Formatter configurations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AngularFireModule } from "@angular/fire";
+import { AngularFireModule } from '@angular/fire';
+import { BrowserModule } from '@angular/platform-browser';
 
-
+import { DatePipe } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FirebaseModule } from './shared/firebase.module';
-import {environment} from '../environments/environment'
-import { MarxaModule } from './shared/marxa.module';
 import { MaterialModule } from './shared/material.module';
-import { TestComponent } from './test/test.component';
-import { DatePipe } from '@angular/common';
 import { StorageComponent } from './test/storage/storage.component';
-// import { MxDevkitModule } from './mx-devkit/marxa-devkit.module';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TestComponent,
-    StorageComponent
-  ],
+  declarations: [AppComponent, TestComponent, StorageComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     FirebaseModule,
     MaterialModule,
-    MarxaModule,
     AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
-  providers: [
-    DatePipe
-  ],
+  providers: [DatePipe],
   schemas: [
     // CUSTOM_ELEMENTS_SCHEMA
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}

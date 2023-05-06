b2dddca32d0354c2d7a30aecfafd623890e395f1
Jorge Guzman <jgu7man@gmail.com>
2023-05-06 08:25:59 -0600

    build(notifications): added submodule

--- projects/marxa/fire-notifications/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of fire-notifications
- */
-
-export * from './lib/fire-notifications.service';
-export * from './lib/fire-notifications.component';
-export * from './lib/fire-notifications.module';
